c4cdb8614ec0c8c5d7b88987c2c3c39b4ca26819
KellyEgesa <kelly.egesa@gmail.com>
2020-11-20 16:54:37 +0300

    Added a test to check if amoung the input for the decoding there was a number

--- src/main/java/CaesarCipher.java
+++ src/main/java/CaesarCipher.java
@@ -2,8 +2,19 @@ public class CaesarCipher {
     private String userInput;
     private int shiftKey;
 
-    public CaesarCipher(String input, int key) {
-        userInput = input;
+    public CaesarCipher(String input, int key) throws Exception {
+        char[] checkNumbers = input.toCharArray();
+        Boolean numberFound = false;
+        for (char checkNumber : checkNumbers) {
+            if (Character.isDigit(checkNumber)) {
+                numberFound = true;
+            }
+        }
+        if (numberFound) {
+            throw new Exception("The input contains a number");
+        } else {
+            userInput = input;
+        }
         shiftKey = key;
     }
 
@@ -14,4 +25,9 @@ public class CaesarCipher {
     public int getShiftKey() {
         return shiftKey;
     }
+
+    public Boolean check() {
+        return Boolean.parseBoolean(getUserInput());
+    }
+
 }
--- src/test/java/CaesarCipherTest.java
+++ src/test/java/CaesarCipherTest.java
@@ -4,19 +4,32 @@ import static org.junit.Assert.*;
 
 public class CaesarCipherTest {
     @Test
-    public void newCaesarCipher_instantiatesCorrectly(){
+    public void newCaesarCipher_instantiatesCorrectly() throws Exception{
         CaesarCipher testCaesarCipher = new CaesarCipher("Boy", 2);
         assertTrue(testCaesarCipher instanceof CaesarCipher);
     }
 
+    @Test(expected = Exception.class)
+    public void newCaesarCipher_ifUserInputString_inputStringOnly() throws Exception{
+        CaesarCipher testCaesarCipher = new CaesarCipher("1", 2);
+        assertEquals(null, testCaesarCipher.getUserInput());
+    }
+
     @Test
-    public void newCaesarCipher_getUserInput_boy() {
+    public void newCaesarCipher_ifShiftKeyInteger_true() throws Exception{
+        CaesarCipher testCaesarCipher = new CaesarCipher("Boy", 2);
+        int testShiftKey = testCaesarCipher.getShiftKey();
+        assertTrue( testShiftKey % 1 == 0 );
+    }
+
+    @Test
+    public void newCaesarCipher_getUserInput_boy() throws Exception {
         CaesarCipher testCaesarCipher = new CaesarCipher("Boy", 2);
         assertEquals("Boy", testCaesarCipher.getUserInput());
     }
 
     @Test
-    public void newCaesarCipher_getShiftKey_2() {
+    public void newCaesarCipher_getShiftKey_2() throws Exception {
         CaesarCipher testCaesarCipher = new CaesarCipher("Boy", 2);
         assertEquals(2, testCaesarCipher.getShiftKey());
     }
