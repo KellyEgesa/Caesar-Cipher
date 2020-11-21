2ee47d29d14b114427c50eae4906d2cdcf0e2256
KellyEgesa <kelly.egesa@gmail.com>
2020-11-21 22:57:00 +0300

    Initialised the front end

---
+++ src/main/java/App.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args){
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("=========================");
+        System.out.println("======CAESAR CIPHER======");
+    }
+}
