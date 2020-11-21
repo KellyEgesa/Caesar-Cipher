db646330af82c68178ed6a7a6abc000281b1090d
KellyEgesa <kelly.egesa@gmail.com>
2020-11-21 23:40:48 +0300

    Added output messages

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class App {
@@ -5,5 +6,11 @@ public class App {
         Scanner myScanner = new Scanner(System.in);
         System.out.println("=========================");
         System.out.println("======CAESAR CIPHER======");
+        System.out.println("=========WELCOME=========");
+        try{
+            System.out.print("Kindly enter your ");
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
